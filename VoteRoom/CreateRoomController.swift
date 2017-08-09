b058aa59ded14c5ddddbd9d7e258b65ce3400510
Marina Tassi <marinaltassi@gmail.com>
2017-08-09 13:41:58 -0400

    Fix for slow connection
    Slow connection made it so app tried to pull current user before
    logging in. Fix was to wait until login complete

--- VoteRoom/CreateRoomController.swift
+++ VoteRoom/CreateRoomController.swift
@@ -5,7 +5,6 @@
 //  Created by Marina Tassi on 8/2/17.
 //  Copyright © 2017 Marina Tassi. All rights reserved.
 //
-
 import UIKit
 import RealmSwift
 
@@ -32,23 +31,34 @@ class CreateRoomController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-        //NOTE that until server is on remote server, we must change the server given below with the current IP
+        //Logout all users
+        let all = SyncUser.all
+        for user in all{
+            user.value.logOut()
+        }
+        print("yay")
+
+        
         let url = URL(string: "http://165.227.86.55:9080")
+        let test = UIApplication.shared.canOpenURL(url!)
+        print(test)
         
         //Authenticate user to create synchronized realm with
         let username = "VRHost"
         let usernameCredentials = SyncCredentials.usernamePassword(username: username, password: "password")
         
+        
         SyncUser.logIn(with: usernameCredentials,
                        server: url!) { user, error in
+                        print("This statement will not print")
                         //if user exists, open synchronized Realm with this user
                         if let user = user {
                             print(user)
                         }
-                        //if user DNE, register a new user to host the synchronized realm
+                            //if user DNE, register a new user to host the synchronized realm
                         else if let error = error {
                             print("User DNE error" + error.localizedDescription)
                             
@@ -64,55 +74,65 @@ class CreateRoomController: UIViewController {
                                                 // Any changes made to this Realm will be synced across all devices!
                                                 print(user)
                                             }
-                                            //You shouldn't run into an error but it is necessary for method
+                                                //You shouldn't run into an error but it is necessary for method
                                             else if let error = error {
                                                 // handle error
                                                 print("Don't know how you got here error" + error.localizedDescription)
                                             }
                             }
                         }
+                        else{
+                            print("NOT DOING ANYTHING")
+                        }
         }
-        
-        //After authenticating the host user, we need to get into their realm
-        let user = SyncUser.current!
-        print(user)
-        
-        // Create the configuration
-        let syncServerURL = URL(string: "realm://165.227.86.55:9080/~/voteRealm")!
-        let config = Realm.Configuration(syncConfiguration: SyncConfiguration(user: user, realmURL: syncServerURL))
-        
-        // Open the remote Realm
-        realm = try! Realm(configuration: config)
-        
-//        let myUsername = UIDevice.current.identifierForVendor!.uuidString
+        //        let myUsername = UIDevice.current.identifierForVendor!.uuidString
         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let viewController = segue.destination as? ViewController {
+            let user = SyncUser.current!
+            print(user)
+            
+            // Create the configuration
+            let syncServerURL = URL(string: "realm://165.227.86.55:9080/~/voteRealm")!
+            let config = Realm.Configuration(syncConfiguration: SyncConfiguration(user: user, realmURL: syncServerURL))
+            
+            // Open the remote Realm
+            realm = try! Realm(configuration: config)
+            
             viewController.realm = realm
         }
         
         if let hostController = segue.destination as? HostController {
+            let user = SyncUser.current!
+            print(user)
+            
+            // Create the configuration
+            let syncServerURL = URL(string: "realm://165.227.86.55:9080/~/voteRealm")!
+            let config = Realm.Configuration(syncConfiguration: SyncConfiguration(user: user, realmURL: syncServerURL))
+            
+            // Open the remote Realm
+            realm = try! Realm(configuration: config)
+            
             hostController.realm = realm
         }
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
